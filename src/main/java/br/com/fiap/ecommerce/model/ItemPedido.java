d82ccf54633ef74b8302b5cd3d4ae205059df8c5
EDUARDO LIMA <weduardow123@gmail.com>
2024-10-20 12:58:12 -0300

    created mappers to all entities + changed all entities to lombock

--- src/main/java/br/com/fiap/ecommerce/model/ItemPedido.java
+++ src/main/java/br/com/fiap/ecommerce/model/ItemPedido.java
@@ -5,8 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class ItemPedido {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -22,44 +24,4 @@ public class ItemPedido {
 
     @Column
     private double valor_total;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId_pedido() {
-        return id_pedido;
-    }
-
-    public void setId_pedido(Long id_pedido) {
-        this.id_pedido = id_pedido;
-    }
-
-    public Long getId_produto() {
-        return id_produto;
-    }
-
-    public void setId_produto(Long id_produto) {
-        this.id_produto = id_produto;
-    }
-
-    public int getQuantidade() {
-        return quantidade;
-    }
-
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
-    }
-
-    public double getValor_total() {
-        return valor_total;
-    }
-
-    public void setValor_total(double valor_total) {
-        this.valor_total = valor_total;
-    }
 }
--- src/main/java/br/com/fiap/ecommerce/model/Pedido.java
+++ src/main/java/br/com/fiap/ecommerce/model/Pedido.java
@@ -7,8 +7,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Pedido {
     
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
@@ -28,52 +30,4 @@ public class Pedido {
 
     @Column
     private double valor_total;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getData_pedido() {
-        return data_pedido;
-    }
-
-    public void setData_pedido(LocalDateTime data_pedido) {
-        this.data_pedido = data_pedido;
-    }
-
-    public String getForma_pagamento() {
-        return forma_pagamento;
-    }
-
-    public void setForma_pagamento(String forma_pagamento) {
-        this.forma_pagamento = forma_pagamento;
-    }
-
-    public Long getId_cliente() {
-        return id_cliente;
-    }
-
-    public void setId_cliente(Long id_cliente) {
-        this.id_cliente = id_cliente;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public double getValor_total() {
-        return valor_total;
-    }
-
-    public void setValor_total(double valor_total) {
-        this.valor_total = valor_total;
-    }
 }
